6e17e5579f782b497aef70bb202b0d3bfac3cc7a
Enteroameba <92228028+Enteroameba@users.noreply.github.com>
2023-08-19 11:45:56 -0600

    add: installer for requirements list

---
+++ installer.py
@@ -0,0 +1,10 @@
+import subprocess
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+for package in requirements:
+    try:
+        subprocess.check_call(["pip", "install", package])
+    except subprocess.CalledProcessError:
+        print(f"Skipping installation of {package} due to an error.")
