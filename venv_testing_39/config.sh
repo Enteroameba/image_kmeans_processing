4925a8c60ac253d8d19bce45a5e78ad599798212
Enteroameba <92228028+Enteroameba@users.noreply.github.com>
2023-09-02 09:13:24 -0600

    delete: venv deprecated

--- venv_testing_39/config.sh
+++
@@ -1,9 +0,0 @@
-
-cd venv_testing_39
-#se puede ejecutar directamente desde la terminal a partir de esta linea
-mkdir docs
-# New-Item es el equivalente a `touch` en mac 
-# `;` es el equivalente a `&`
-mkdir scripts ; New-Item scripts/__init__.py
-mkdir notebooks
-mkdir data ; cd data ; mkdir raw ; mkdir processed ; cd ..
